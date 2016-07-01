9218700083babe8814c1e18e4cae94daed373ef8
Caley Davis <caleyd@gmail.com>
2016-06-30 18:39:49 -0700

    refactor and simplify - consolidate app to single screen

--- index.ios.js
+++ index.ios.js
@@ -1,97 +1,42 @@
-import React, { Component, PropTypes } from 'react';
-import ReactNative, {
+import React, { Component } from 'react';
+import {
   AppRegistry,
-  Text,
-  TouchableHighlight,
-  View,
   NavigatorIOS
 } from 'react-native';
-import NumberInput from './lib/react-native-numberinput';
-import PureComponent from './lib/pureComponent';
 import OutlookTablePage from './lib/outlookPage';
 import styles from './lib/styles.js';
 
-var scenarioStore = require('./lib/scenarioStore');
-var calc = require('./lib/calculator.js');
-import formatMoneyCompact from './lib/formatMoney';
-
-class InputFormInputRow extends PureComponent {
+class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: parseInt(props.value || 0)};
+    this.statics = {
+      title: '<NavigatorIOS>',
+      description: 'Early Retirement Calculator',
+      external: true,
+    };
   }
+
   render() {
     return (
-      <View style={[styles.scenarioFormRow, {height: 44}]}>
-        <Text style={styles.scenarioFormRowLabel}>{this.props.labelText}</Text>
-        {(this.props.type === 'dollar' ?
-          <NumberInput value={this.state.value} inputStyle={styles.scenarioFormRowInput}
-              onChange={(num)=>this.onChange(num)}
-              getFormattedText={(num)=>formatMoneyCompact(num)}
-              />
-            : this.props.type === 'percent' ?
-          <NumberInput value={this.state.value} inputStyle={styles.scenarioFormRowInput}
-              onChange={(num)=>this.onChange(num)}
-              keyboardType={'decimal-pad'}
-              getFormattedText={(num)=>this.formatPercent(num)}
-              />
-            :
-          <NumberInput value={this.state.value} inputStyle={styles.scenarioFormRowInput}
-              onChange={(num)=>this.onChange(num)}
-              />
-        )}
-      </View>
-    );
-  }
-  formatPercent(value) {
-    return (value * 100).toFixed(2) + '%'
-  }
-  onChange(num) {
-    this.setState({value: num});
-    this.props.onChange(num);
-  }
-}
-InputFormInputRow.propTypes = {
-  value: PropTypes.number.isRequired,
-  labelText: PropTypes.string.isRequired,
-  type: PropTypes.oneOf(['dollar', 'percent', 'years']),
-  onChange: PropTypes.func
-};
-
-class MarketAssumptions extends PureComponent {
-  render() {
-    return (
-      <View style={styles.card}>
-        <View style={[styles.cardHeader, styles.roundedTop]}>
-          <Text style={{ backgroundColor: 'transparent' }}>Market Assumptions</Text>
-        </View>
-        {/* TODO - add horizontal scrolling options for: conservative, moderate, aggressive */}
-
-        <InputFormInputRow labelText='Annual Return' type='percent'
-          value={this.props.scenario.annualReturn}
-          onChange={(num)=>scenarioStore.setAnnualReturn(num)}/>
-        <InputFormInputRow labelText='Withdrawal Rate' type='percent'
-          value={this.props.scenario.withdrawalRate}
-          onChange={(num)=>scenarioStore.setWithdrawalRate(num)}/>
-      </View>
-    );
-  }
-}
-MarketAssumptions.propTypes = {
-  scenario: PropTypes.object.isRequired
-};
-
-class Intro extends PureComponent {
-  render() {
-    return (
-      <View>
-        <Text style={styles.welcome}>Early Retirement Calculator!</Text>
-        <Text style={styles.instructions}>Your path to financial independence</Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: this.statics.title,
+          component: OutlookTablePage,
+          navigationBarHidden: true
+        }}
+        itemWrapperStyle={styles.itemWrapper}
+        tintColor="green"
+        barTintColor="green"
+        shadowHidden={true}
+      />
     );
   }
 }
 
+AppRegistry.registerComponent('EarlyRetireCalc', () => App);
+
+/*
 class Outlook extends PureComponent {
   render() {
     var yearsToRetirement = Math.round(10 * this.props.retirementOutlook.yearsToRetirement) / 10;
@@ -134,86 +79,4 @@ class Outlook extends PureComponent {
       });
   }
 }
-
-// Because of limitations of NavigatorIOS - this is acting as the controller view
-class MainScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      initialized: false,
-      scenario: null
-    };
-    scenarioStore.getScenario((err, scenario) => {
-      if(err) {
-        throw new Error(err);
-      }
-      this.setState({ scenario: scenario, initialized: true });
-    });
-    this.scenarioListener = scenarioStore.addListener('change',
-      (scenario) => this.onScenarioChanged(scenario)
-    );
-  }
-
-  componentWillUnmount() {
-    this.scenarioListener.remove();
-  }
-
-  onScenarioChanged(scenario) {
-    this.setState({ scenario });
-  }
-
-  render() {
-    var scenario = this.state.scenario;
-
-    var retirementOutlook = scenario ? calc.calculate(scenario) : null;
-
-    return (
-      <View contentContainerStyle={styles.container} scrollEnabled={false}
-          style={{backgroundColor:'green'}}>
-
-        <Intro/>
-
-        {this.state.initialized ?
-          <MarketAssumptions scenario={scenario}/>
-          : null
-        }
-        {this.state.initialized && retirementOutlook ?
-          <Outlook style={{alignSelf:'flex-end'}} navigator={this.props.navigator}
-            scenario={scenario} retirementOutlook={retirementOutlook}/>
-          : null
-        }
-
-      </View>
-    );
-  }
-}
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.statics = {
-      title: '<NavigatorIOS>',
-      description: 'Early Retirement Calculator',
-      external: true,
-    };
-  }
-
-  render() {
-    return (
-      <NavigatorIOS
-        style={styles.container}
-        initialRoute={{
-          title: this.statics.title,
-          component: MainScreen,
-          navigationBarHidden: true
-        }}
-        itemWrapperStyle={styles.itemWrapper}
-        tintColor="green"
-        barTintColor="green"
-        shadowHidden={true}
-      />
-    );
-  }
-}
-
-AppRegistry.registerComponent('EarlyRetireCalc', () => App);
+*/
