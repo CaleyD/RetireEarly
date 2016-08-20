1dbd7d5d94e6fd844c639ad793e680593d7923f4
Caley Davis <caleyd@gmail.com>
2016-08-20 13:52:47 -0700

    bump dependency versions

--- ios/EarlyRetireCalc/AppDelegate.m
+++ ios/EarlyRetireCalc/AppDelegate.m
@@ -20,7 +20,8 @@
 
   //jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
-  jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.10:8081/index.ios.bundle?platform=ios&dev=true"];
+  jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.10:8081/index.ios.bundle?platform=ios&dev=false"];
+  
   
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"EarlyRetireCalc"
